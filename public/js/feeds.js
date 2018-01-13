ee12c55f7cc08c85e9fd07e17b0c7dd5a49103d7
afshanaman81 <afshan.aman@gmail.com>
2018-01-13 22:28:07 +0400

    commented the test code

--- public/js/feeds.js
+++ public/js/feeds.js
@@ -1,7 +1,7 @@
 
-var instaFeed = new Instafeed({
-	get: 'user',
-	userId: USER_ID, // Ex: 1374300081
-	accessToken: 'YOUR_ACCESS_TOKEN'
-});
-instaFeed.run();
+// var instaFeed = new Instafeed({
+// 	get: 'user',
+// 	userId: USER_ID, // Ex: 1374300081
+// 	accessToken: 'YOUR_ACCESS_TOKEN'
+// });
+// instaFeed.run();
